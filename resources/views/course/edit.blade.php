1c2eb0a3545ac22a1ea3882065dea63056708592
sakhawat09q <98444291+sakhawat09q@users.noreply.github.com>
2022-09-16 12:11:13 -0500

    Update edit.blade.php

--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -14,6 +14,8 @@
             <div class="col-6">
                     <form action="{{URL::to('update-course/'.$course->id)}}" method="post">
                         {{csrf_field()}}
+                   
+                   <!-- course Name update     -->
                 <div class="form-group">
                     <label for="">Course Name</label>
                     <input type="text" class="form-control" name="course_name"value="{{$course->course_name}}" placeholder="Enter course name">
@@ -23,20 +25,43 @@
                     <label for="exampleInputPassword1">Course Code</label>
                     <input type="text" class="form-control" name="course_code"value="{{$course->course_code}}" placeholder="Enter course code">
                 </div>
+
+                <!-- course type update -->
                 <div class="form-group">
                     <label for="">Course Type</label>
                     <select class="form-control" name="course_type"id=""value="{{$course->course_type}}">
                     <option>SELECT TYPE</option>
-                     <option value="CSE"{{$course->course_type=='CSE'? 'selected':''}}>CSE</option>
-                    <option>ECONOMICS</option>
-                    <option>MATH</option>
-                    <option>LAW</option>
+                    <option value="CSE"{{$course->course_type=='CSE'? 'selected':''}}>CSE</option>
+                    <option value="ECONOMICS"{{$course->course_type=='ECONOMICS'? 'selected':''}}>ECONOMICS</option>
+                    <option value="MATH"{{$course->course_type=='MATH'? 'selected':''}}>MATH</option>
+                    <option value="LAW"{{$course->course_type=='LAW'? 'selected':''}}>LAW</option>
                     </select>
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <!-- gender -->
+            <div class="form-group">
+                <label>Gender</label>
+                <br />
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="inlineRadio3" value="Other">
+                    <label class="form-check-label" {{ $course -> Gender == 'Male' ? 'checked' :''}} for="inlineRadio3">Male</label>
                 </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="inlineRadio3" value="Other">
+                    <label class="form-check-label" {{ $course -> Gender == 'Female' ? 'checked' :''}} for="inlineRadio3">Female</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="inlineRadio3" value="Other">
+                    <label class="form-check-label" {{ $course -> Gender == 'other' ? 'checked' :''}} for="inlineRadio3">Other</label>
+                </div>    
+            </div>
+              <!-- Active_status check -->
+                <div class="form-group">
+                  <label>Active Status</label>
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="checkbox" name="active_status" class="form-check-input" checked value="1">Is_Active
+                       </label>
+                    </div>
                 <button type="submit" class="btn btn-success">Update</button>
                 <a href="{{URL::to('courses')}}"type="button" class="btn btn-dark">Courses List</a>
                 
